8d53bef3e3f3e5fa972bea3e5fb67c6c6454517e
eduardoleivas <duduleivas@gmail.com>
2024-04-07 02:33:48 -0300

    fix: github pages #6.1

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/clonegram>
+  <BrowserRouter basename="/clonegram">
     <App />
   </BrowserRouter>
 )
